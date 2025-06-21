56fdb9ecb996c90dbc2b85ba29f8c1d2415b0bdf
mzlat <mzlatin4@gmail.com>
2025-06-21 14:10:40 +0200

    added links

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
 const scoreElem = document.getElementById('score');
 const resetBtn = document.getElementById('resetScore');
 const clickTarget = document.getElementById('clickTarget');
-
+const soundEffect = document.getElementById('soundEffect');
 
 let score = 0;
-
-
 
 chrome.storage.local.get(['score'], (data) => {
   score = data.score || 0;
@@ -19,3 +17,8 @@ resetBtn.addEventListener('click', () => {
   scoreElem.textContent = score;
 });
 
+
+clickTarget.addEventListener('click', () => {
+  soundEffect.currentTime = 0;
+  soundEffect.play();
+});
