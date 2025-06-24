e6288bb19487e8f7c163fc9b06ea26353d84476b
mzlat <mzlatin4@gmail.com>
2025-06-24 20:20:01 +0200

    added links

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB78vYG0vYRzKcz7WxZshmO3ep_OJLnC0Y",
+  authDomain: "polyphem-click.firebaseapp.com",
+  projectId: "polyphem-click",
+  storageBucket: "polyphem-click.firebasestorage.app",
+  messagingSenderId: "789425657065",
+  appId: "1:789425657065:web:519e4284bfa9984b480c78"
+};
