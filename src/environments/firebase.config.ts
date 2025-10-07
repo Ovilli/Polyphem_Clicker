5946dd29f0f03885de61c75f323bf77159236cd3
Ovilli <mzlatin4@gmail.com>
2025-10-07 21:40:02 +0200

    Delete src/environments directory
    Signed-off-by: Ovilli <mzlatin4@gmail.com>

--- src/environments/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyB78vYG0vYRzKcz7WxZshmO3ep_OJLnC0Y",
-  authDomain: "polyphem-click.firebaseapp.com",
-  projectId: "polyphem-click",
-  storageBucket: "polyphem-click.firebasestorage.app",
-  messagingSenderId: "789425657065",
-  appId: "1:789425657065:web:519e4284bfa9984b480c78"
-};
